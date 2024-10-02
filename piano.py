de092271d2a433e148bb080235083a285f1f5775
Giuseppe Bellamacina <giuseppe.bellamacina@studium.unict.it>
2024-10-02 18:56:56 +0200

    MultiNote Fixed

--- piano.py
+++ piano.py
@@ -1,21 +1,29 @@
 import pygame
-import time
-from os import system
-import threading
+import asyncio
+from os import system, name
 
-class Note:
-    def __init__(self, name):
+class Note():
+    def __init__(self, name, duration):
         self.name = name
+        self.duration = duration
+        self.sounds = []
         self.load_sound()
-
+    
     def load_sound(self):
-        self.sound = pygame.mixer.Sound("piano_samples/" + self.name + ".mp3")
-
-    def play(self):
+        notes = self.name.split('+')
+        for note in notes:
+            sound = f'{note}.mp3'
+            self.sounds.append(pygame.mixer.Sound(f'piano_samples/{sound}'))
+    
+    async def play(self):
         print(self.name)
-        self.sound.play()
+        for i, sound in enumerate(self.sounds):
+            pygame.mixer.Channel(i).play(sound)
+        await asyncio.sleep(self.duration)
+        for i, sound in enumerate(self.sounds):
+            pygame.mixer.Channel(i).stop()
 
-def read_score(file_path):
+def read_score(file_path: str) -> list[Note]:
     pygame.mixer.init()
     with open(file_path, 'r') as f:
         lines = f.readlines()
@@ -24,25 +32,16 @@ def read_score(file_path):
         if ',' not in line:
             continue
         notes, duration = line.strip().split(',')
-        notes = [Note(note) for note in notes.split('+')]
-        score.append((notes, float(duration)))
+        note = Note(notes, float(duration))
+        score.append(note)
     return score
 
-def play_notes(score):
-    for notes, duration in score:
-        start_time = time.time()
-        threads = []
-        for note in notes:
-            thread = threading.Thread(target=note.play)
-            thread.start()
-            threads.append(thread)
-        elapsed_time = time.time() - start_time
-        time.sleep(max(0, duration - elapsed_time))
-        for thread in threads:
-            thread.join()
+def play_notes(score: list[Note]):
+    for notes in score:
+        asyncio.run(notes.play())
 
 if __name__ == '__main__':
-    system("cls")
-    score = read_score('spartito.txt')
+    system('clear') if name == 'posix' else system('cls')
+    score = read_score('lalaland.txt')
     play_notes(score)
     pygame.mixer.quit()
